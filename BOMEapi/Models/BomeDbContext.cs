e20dc3f8f97f02a5e7842d79220ba51c1acebb69
Guilherme de Barros da Rocha <guilhermebarrosr@hotmail.com>
2019-05-08 12:48:54 -0300

    atualizações

--- BOMEapi/Models/BomeDbContext.cs
+++ BOMEapi/Models/BomeDbContext.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using BOMEapi.Models;
 using MySql.Data.MySqlClient;
 using Microsoft.Extensions.Configuration;
 using System.IO;
